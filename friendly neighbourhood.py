3e8244975aec43a5f0ca44370c7104143c459a96
pgsdananjani <dananjanishanika@gmail.com>
2020-10-10 13:01:29 +0530

    friendly neighborhood pushed to alpha

---
+++ friendly neighbourhood.py
@@ -0,0 +1,30 @@
+
+neighbourArr = [10,3,2,5,7,1]
+
+copyneighbourArr = list(neighbourArr)
+
+copyneighbourArr.sort(reverse=True)
+
+length = len(neighbourArr)
+
+non = 0
+
+for n in copyneighbourArr :
+    non += n
+    x = neighbourArr.index(n)
+    p = x-1
+    q = x+1
+    if p >= 0:
+        if neighbourArr[p] in copyneighbourArr:     
+            copyneighbourArr.remove(neighbourArr[p])
+    if q <= (length - 1):
+        if neighbourArr[q] in copyneighbourArr:
+            copyneighbourArr.remove(neighbourArr[q])
+
+
+print(non)
+        
+    
+        
+
+
